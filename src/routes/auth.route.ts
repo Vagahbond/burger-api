ce9568fc814d70a5f456bb11c3668805eb0013c5
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-20 23:06:17 +0100

    Encapsulate /auth/register in try...catch

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -10,40 +10,47 @@ import * as models from '../models'
 import * as security from '../utils/security.utils'
 
 router.post('/auth/register', async (req, res) => {
-    const { firstname, lastname, email, password } = req.body || {}
+    try {
+        const { firstname, lastname, email, password } = req.body || {}
 
-    const errors: string[] = []
+        const errors: string[] = []
 
-    if (typeof firstname !== 'string' || !XRegExp('^[\\p{L}\- ]{2,}$').test(firstname.trim()))
-        errors.push("Invalid firstname.")
+        if (typeof firstname !== 'string' || !XRegExp('^[\\p{L}\- ]{2,}$').test(firstname.trim()))
+            errors.push("Invalid firstname.")
 
-    if (typeof lastname !== 'string' || !XRegExp('^[\\p{L}\- ]{2,}$').test(lastname.trim()))
-        errors.push("Invalid lastname.")
+        if (typeof lastname !== 'string' || !XRegExp('^[\\p{L}\- ]{2,}$').test(lastname.trim()))
+            errors.push("Invalid lastname.")
 
-    if (typeof email !== 'string' || !validator.isEmail(email))
-        errors.push("Invalid email address.")
-    else if (await models.user.model.exists({ email }))
-        errors.push("Email address already used.")
+        if (typeof email !== 'string' || !validator.isEmail(email))
+            errors.push("Invalid email address.")
+        else if (await models.user.model.exists({ email }))
+            errors.push("Email address already used.")
 
-    if (typeof password !== 'string' || password.length < 5)
-        errors.push("Invalid password (minimum 5 characters needed).")
+        if (typeof password !== 'string' || password.length < 5)
+            errors.push("Invalid password (minimum 5 characters needed).")
 
-    if (errors.length > 0) {
-        return res.status(400).json({
+        if (errors.length > 0) {
+            return res.status(400).json({
+                success: false,
+                errors,
+            })
+        }
+
+        const user = await models.user.model.create({
+            firstname: firstname.trim().replace(/  +/g, ''),
+            lastname: lastname.trim().replace(/  +/g, ''),
+            email,
+            password: await security.hash(password),
+        })
+
+        res.json({
+            success: true,
+            user: models.user.sanitize_user(user),
+        })
+    } catch (e) {
+        res.status(500).json({
             success: false,
-            errors,
+            error: "Failed to register."
         })
     }
-
-    const user = await models.user.model.create({
-        firstname: firstname.trim().replace(/  +/g, ''),
-        lastname: lastname.trim().replace(/  +/g, ''),
-        email,
-        password: await security.hash(password),
-    })
-
-    res.json({
-        success: true,
-        user: models.user.sanitize_user(user),
-    })
 })
