85b339be10dcafd06c4676f197aed7684ff8bedb
Stanislas Lange <angristan@pm.me>
2020-03-20 17:31:28 +0100

    Update models

--- src/models/menu.model.ts
+++ src/models/menu.model.ts
@@ -2,9 +2,9 @@ import db from '../db'
 
 export const schema = new db.Schema({
     name: { type: String, required: true, },
-    products: {type: db.Schema.Types.ObjectId, ref: 'Product', required: true},
+    products: [{type: db.Schema.Types.ObjectId, ref: 'Product', required: true}],
     price: { type: Number, required: true, },
-    promotion: { type: Number, required: true, },
+    promotion: { type: Number, required: true, default: 0 },
 })
 
 export const model = db.model('Menu', schema)
