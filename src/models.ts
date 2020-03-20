11874e18e802a5ccedeb80b08c200140728c65f3
Stanislas Lange <angristan@pm.me>
2020-03-20 17:16:56 +0100

    Add menu model

--- src/models.ts
+++ src/models.ts
@@ -1,3 +1,4 @@
 export * as user from './models/user.model'
 export * as product from './models/product.model'
 export * as order from './models/order.model'
+export * as menu from './models/menu.model'
---
+++ src/models/menu.model.ts
@@ -0,0 +1,10 @@
+import db from '../db'
+
+export const schema = new db.Schema({
+    name: { type: String, required: true, },
+    products: {type: db.Schema.Types.ObjectId, ref: 'Product', required: true},
+    price: { type: Number, required: true, },
+    promotion: { type: Number, required: true, },
+})
+
+export const model = db.model('Menu', schema)
