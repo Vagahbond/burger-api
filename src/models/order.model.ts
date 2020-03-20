20d6353f8744a046bbfae328b0fe69c6d927b0ad
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-20 23:08:39 +0100

    Fix orders indentation

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -6,11 +6,10 @@ enum OrderStatus {
     Cancelled
 }
 
-
 export const schema = new db.Schema({
     status: { type: OrderStatus, required: true, default: OrderStatus.InProgress, },
-    customer: {type: db.Schema.Types.ObjectId, ref: 'User', required: true},
-    products: [{type: db.Schema.Types.ObjectId, ref: 'Product', required: true}],
+    customer: { type: db.Schema.Types.ObjectId, ref: 'User', required: true },
+    products: [{ type: db.Schema.Types.ObjectId, ref: 'Product', required: true }],
     creation_date: { type: Date, required: true, default: Date.now() },
     withdrawal_date: { type: Date, required: true, },
 })
