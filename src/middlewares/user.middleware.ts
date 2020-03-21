0caf2dad6afd78f6c338245c56f049f1ce538d2e
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-21 17:47:03 +0100

    Fix connected user fetching

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -5,7 +5,8 @@ import * as models from '../models'
 export default async (req: Request, res: Response, next: NextFunction) => {
     if (req.token) {
         try {
-            req.user = await models.token.model.findOne({ token: req.token }).populate('user').lean() as models.user.IUser
+            const token = await models.token.model.findOne({ token: req.token }).populate('user').lean()
+            req.user = token?.user as models.user.IUser
         } catch { }
     }
 
