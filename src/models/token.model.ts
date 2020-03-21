fd4700c81c1438b62b871b5abfc51a565856ae03
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-21 17:46:37 +0100

    Token interface's user property can be IUser

--- src/models/token.model.ts
+++ src/models/token.model.ts
@@ -1,8 +1,10 @@
 import db from '../db'
 
+import { IUser } from './user.model'
+
 export interface IToken extends db.Document {
     token: string;
-    user: db.Schema.Types.ObjectId;
+    user: db.Schema.Types.ObjectId | IUser;
 }
 
 export const schema = new db.Schema({
