50d3382d909e47db816c108149d0c4572d5f4474
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-22 13:25:33 +0100

    Implement Joi for auth routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,48 +3,59 @@ const router = Router()
 export default router
 
 import crypto from 'crypto'
-
-import validator from 'validator'
-import XRegExp from 'xregexp'
+import Joi from '@hapi/joi'
 
 import * as models from '../models'
+import * as security from '../utils/security.utils'
 
 import guard from '../middlewares/guard.middleware'
 
-import * as security from '../utils/security.utils'
+interface IAuthRegisterPost {
+    firstname: string
+    lastname: string
+    email: string
+    password: string
+}
+
+const auth_register_post_schema = Joi.object<IAuthRegisterPost>().options({
+    abortEarly: false,
+    stripUnknown: true,
+}).keys({
+    firstname: Joi.string().pattern(/^[\p{L}\- ]{2,}$/u).required().messages({
+        'string.base': `'firstname' should be a string`,
+        'string.empty': `'firstname' cannot be empty`,
+        'string.pattern': `'firstname' is invalid`,
+        'any.required': `'firstname' is a required field`
+    }),
+    lastname: Joi.string().pattern(/^[\p{L}\- ]{2,}$/u).required().messages({
+        'string.base': `'lastname' should be a string`,
+        'string.empty': `'lastname' cannot be empty`,
+        'string.pattern': `'lastname' is invalid`,
+        'any.required': `'lastname' is a required field`
+    }),
+    email: Joi.string().email().required().messages({
+        'string.base': `'email' should be a string`,
+        'string.empty': `'email' cannot be empty`,
+        'string.email': `'email' is an invalid email address`,
+        'any.required': `'email' is a required field`
+    }),
+    password: Joi.string().min(5).required().messages({
+        'string.base': `'password' should be a string`,
+        'string.empty': `'password' cannot be empty`,
+        'string.min': `'password' should have a minimum length of {#limit}`,
+        'any.required': `'password' is a required field`
+    }),
+})
 
 router.post('/auth/register', guard({ auth: false }), async (req, res) => {
     try {
-        const { firstname, lastname, email, password } = req.body || {}
-
-        const errors: string[] = []
-
-        if (typeof firstname !== 'string' || !XRegExp('^[\\p{L}\- ]{2,}$').test(firstname.trim()))
-            errors.push("Invalid firstname.")
-
-        if (typeof lastname !== 'string' || !XRegExp('^[\\p{L}\- ]{2,}$').test(lastname.trim()))
-            errors.push("Invalid lastname.")
-
-        if (typeof email !== 'string' || !validator.isEmail(email))
-            errors.push("Invalid email address.")
-        else if (await models.user.model.exists({ email }))
-            errors.push("Email address already used.")
-
-        if (typeof password !== 'string' || password.length < 5)
-            errors.push("Invalid password (minimum 5 characters needed).")
-
-        if (errors.length > 0) {
-            return res.status(400).json({
-                success: false,
-                errors,
-            })
-        }
+        const data: IAuthRegisterPost = await auth_register_post_schema.validateAsync(req.body)
 
         const user = await models.user.model.create({
-            firstname: firstname.trim().replace(/  +/g, ''),
-            lastname: lastname.trim().replace(/  +/g, ''),
-            email,
-            password: await security.hash(password),
+            firstname: data.firstname.trim().replace(/  +/g, ''),
+            lastname: data.lastname.trim().replace(/  +/g, ''),
+            email: data.email,
+            password: await security.hash(data.password),
         })
 
         res.json({
@@ -52,6 +63,13 @@ router.post('/auth/register', guard({ auth: false }), async (req, res) => {
             user: models.user.sanitize_user(user),
         })
     } catch (e) {
+        if ('details' in e) {
+            return res.status(400).json({
+                success: false,
+                errors: (e as Joi.ValidationError).details.map(d => d.message),
+            })
+        }
+
         res.status(500).json({
             success: false,
             error: "Failed to register."
@@ -59,36 +77,43 @@ router.post('/auth/register', guard({ auth: false }), async (req, res) => {
     }
 })
 
+interface IAuthLoginPost {
+    email: string
+    password: string
+}
+
+const auth_login_post_schema = Joi.object<IAuthLoginPost>().options({
+    abortEarly: false,
+    stripUnknown: true,
+}).keys({
+    email: Joi.string().email().required().messages({
+        'string.base': `'email' should be a string`,
+        'string.empty': `'email' cannot be empty`,
+        'string.email': `'email' is an invalid email address`,
+        'any.required': `'email' is a required field`
+    }),
+    password: Joi.string().min(5).required().messages({
+        'string.base': `'password' should be a string`,
+        'string.empty': `'password' cannot be empty`,
+        'any.required': `'password' is a required field`
+    }),
+})
+
 router.post('/auth/login', guard({ auth: false }), async (req, res) => {
     try {
-        const { email, password } = req.body || {}
-
-        const errors: string[] = []
-
-        if (typeof email !== 'string' || !validator.isEmail(email))
-            errors.push("Invalid email address.")
-
-        if (errors.length > 0) {
-            return res.status(400).json({
-                success: false,
-                errors,
-            })
-        }
+        const data: IAuthLoginPost = await auth_login_post_schema.validateAsync(req.body)
 
         const invalid_credentials = () => res.status(400).json({
             success: false,
             error: "Invalid credentials."
         })
 
-        if (typeof password !== 'string')
-            return invalid_credentials()
-
-        const user = await models.user.model.findOne({ email }).lean()
+        const user = await models.user.model.findOne({ email: data.email }).lean()
 
         if (!user)
             return invalid_credentials()
 
-        const valid = await security.compare_hash(user.password, password)
+        const valid = await security.compare_hash(user.password, data.password)
         if (!valid)
             return invalid_credentials()
 
@@ -102,10 +127,16 @@ router.post('/auth/login', guard({ auth: false }), async (req, res) => {
             token: token.token,
         })
     } catch (e) {
+        if ('details' in e) {
+            return res.status(400).json({
+                success: false,
+                errors: (e as Joi.ValidationError).details.map(d => d.message),
+            })
+        }
+
         res.status(500).json({
             success: false,
             error: "Failed to login."
         })
     }
 })
-
