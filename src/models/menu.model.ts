66a09dd316e2b676df9e15b7656a0e955bc23797
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-22 18:54:02 +0100

    Add menu interface

--- src/models/menu.model.ts
+++ src/models/menu.model.ts
@@ -1,10 +1,19 @@
 import db from '../db'
 
+import { IProduct } from './product.model'
+
+interface IMenu {
+    name: string,
+    products: (db.Schema.Types.ObjectId | IProduct)[]
+    price: number
+    promotion: number
+}
+
 export const schema = new db.Schema({
     name: { type: String, required: true, },
-    products: [{type: db.Schema.Types.ObjectId, ref: 'Product', required: true}],
+    products: [{ type: db.Schema.Types.ObjectId, ref: 'Product', required: true }],
     price: { type: Number, required: true, },
     promotion: { type: Number, required: true, default: 0 },
 })
 
-export const model = db.model('Menu', schema)
+export const model = db.model<IMenu & db.Document>('Menu', schema)
