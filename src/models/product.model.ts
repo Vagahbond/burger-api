5c30944d1e241c55327a05a9038b6a00176682ef
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-22 18:51:09 +0100

    Add product interface

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,5 +1,12 @@
 import db from '../db'
 
+export interface IProduct {
+    name: string
+    count: number
+    price: number
+    promotion: number
+}
+
 export const schema = new db.Schema({
     name: { type: String, required: true, },
     count: { type: Number, required: true, },
@@ -7,7 +14,7 @@ export const schema = new db.Schema({
     promotion: { type: Number, required: true, },
 })
 
-export const model = db.model('Product', schema)
+export const model = db.model<IProduct & db.Document>('Product', schema)
 
 export function sanitize_product(product: any) {
     if (!product) return null
