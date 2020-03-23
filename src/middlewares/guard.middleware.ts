4567640d2d0a28a50c4d871b77c3a953aa4010e9
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-23 21:08:17 +0100

    Add connected user auth guard

--- src/middlewares/guard.middleware.ts
+++ src/middlewares/guard.middleware.ts
@@ -3,13 +3,21 @@ import { Request, Response, NextFunction } from 'express'
 import * as models from '../models'
 
 interface GuardOptions {
-    auth?: false
+    auth?: boolean
     allow?: models.user.UserLevel[]
+    deny?: models.user.UserLevel[]
 }
 
 export default (options: GuardOptions) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        if (options.auth === undefined) {
+        if (options.auth === true) {
+            if ((typeof req.user?.level !== 'number') || options.deny?.includes(req.user.level)) {
+                return res.status(401).json({
+                    success: false,
+                    error: 'Unauthorized.',
+                })
+            }
+        } else if (options.auth === undefined) {
             if ((typeof req.user?.level !== 'number') || !options.allow?.includes(req.user.level)) {
                 return res.status(401).json({
                     success: false,
