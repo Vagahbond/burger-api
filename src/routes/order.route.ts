dbb74bedc6ae771fe3a842e84a1f71bdee82affa
quantumsheep <nathanael.dmc@outlook.fr>
2020-05-10 15:14:31 +0200

    Add /orders/:id route

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -3,9 +3,12 @@ const router = Router()
 
 export default router
 
+import Joi from '@hapi/joi'
+
 import * as models from '../models'
 
 import guard from '../middlewares/guard.middleware'
+import schema from '../middlewares/schema.middleware'
 
 import { UserLevel } from '../models/user.model'
 
@@ -29,3 +32,34 @@ router.get('/orders', guard({
         })
     }
 })
+
+router.get(
+    '/order/:id',
+    schema({
+        params: Joi.object({
+            id: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
+        }),
+    }),
+    guard({
+        allow: [UserLevel.Admin, UserLevel.Preparator],
+    }),
+    async (req, res) => {
+        try {
+            const order = await models.order.model.findOne({
+                _id: req.params.id,
+            })
+
+            res.json({
+                success: true,
+                order,
+            })
+        }
+        catch (err) {
+            console.error(err)
+
+            res.status(500).json({
+                succes: false,
+                error: 'Error while fetching the order.'
+            })
+        }
+    })
