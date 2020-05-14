66d2f3be4498385dd983e3a6cc77fe7a52a411e2
quantumsheep <nathanael.dmc@outlook.fr>
2020-05-14 16:32:46 +0200

    Fix product modification

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -5,29 +5,8 @@ export default router
 import Joi from '@hapi/joi'
 
 import * as models from '../models'
-
-const schema = Joi.object({
-    name: Joi.string()
-        .alphanum()
-        .min(1)
-        .required(),
-
-    count: Joi.number()
-        .integer()
-        .min(0)
-        .required(),
-
-    price: Joi.number()
-        .integer()
-        .min(0)
-        .required(),
-
-    promotion: Joi.number()
-        .integer()
-        .min(0)
-        .max(100)
-        .required(),
-})
+import schema from '../middlewares/schema.middleware'
+import { IProduct } from '../models/product.model'
 
 router.get('/products', async (req, res) => {
     try {
@@ -67,9 +46,33 @@ router.get('/product/:id', async (req, res) => {
     }
 })
 
+
+const create_product_schema = Joi.object({
+    name: Joi.string()
+        .alphanum()
+        .min(1)
+        .required(),
+
+    count: Joi.number()
+        .integer()
+        .min(0)
+        .required(),
+
+    price: Joi.number()
+        .integer()
+        .min(0)
+        .required(),
+
+    promotion: Joi.number()
+        .integer()
+        .min(0)
+        .max(100)
+        .required(),
+})
+
 router.post('/product', async (req, res) => {
     try {
-        const received_product = await schema.validateAsync(req.body)
+        const received_product = await create_product_schema.validateAsync(req.body)
 
         const validated_product = new models.product.model(received_product)
 
@@ -87,37 +90,64 @@ router.post('/product', async (req, res) => {
     }
 })
 
-router.patch('/product/:id', async (req, res) => {
-    try {
-        const searched_product = await models.product.model.findOne({ _id: req.params.id })
-        if (searched_product === null) {
-            res.status(404).json({
+const product_put_schema = Joi.object({
+    name: Joi.string()
+        .alphanum()
+        .min(1),
+
+    count: Joi.number()
+        .integer()
+        .min(0),
+
+    price: Joi.number()
+        .integer()
+        .min(0),
+
+    promotion: Joi.number()
+        .integer()
+        .min(0)
+        .max(100),
+})
+
+router.put('/product/:id',
+    schema({
+        body: product_put_schema,
+        params: Joi.object({
+            id: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
+        }),
+    }),
+    async (req, res) => {
+        try {
+            const searched_product = await models.product.model.findOne({ _id: req.params.id })
+            if (searched_product === null) {
+                res.status(404).json({
+                    success: false,
+                    error: 'Product was not found.',
+                })
+            } else {
+                const body = req.body as IProduct
+
+                for (const k in body) {
+                    type KVObject = { [key: string]: any }
+                    (searched_product as KVObject)[k] = (body as KVObject)[k]
+                }
+
+                const saved_product = await searched_product.save()
+
+                res.json({
+                    success: true,
+                    product: models.product.sanitize_product(saved_product),
+                })
+            }
+        } catch (err) {
+            console.error(err)
+
+            res.status(400).json({
                 success: false,
-                error: 'Product was not found.',
-            })
-        } else {
-            const received_product = await schema.validateAsync(req.body)
-
-            // Merge current product with updated properties
-            const modified_product = { ...searched_product, ...received_product }
-
-            // Make sure the product is still valid
-            const validated_product = new models.product.model(modified_product)
-
-            const saved_product = await validated_product.save()
-
-            res.json({
-                success: true,
-                product: models.product.sanitize_product(saved_product),
+                error: 'Unable to update product',
             })
         }
-    } catch (err) {
-        res.status(400).json({
-            success: false,
-            error: 'Unable to update product',
-        })
-    }
-})
+    })
 
 router.get('/products/featured', async (req, res) => {
     try {
